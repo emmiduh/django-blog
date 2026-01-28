9c424e909416501c62a2a83e0b3169b6d284c8fd
emmiduh <emmiduh93@gmail.com>
2026-01-28 14:34:46 +0200

    update blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -31,13 +31,3 @@ urlpatterns = [
     path('feed/', LatestPostsFeed(), name='post_feed'),
     path('search/', views.post_search, name='post_search'),
 ]
-
-EMAIL_HOST_USER=emmiduh93@gmail.com
-EMAIL_HOST_PASSWORD=cnvrvgvtqwggbkdr
-DEFAULT_FROM_EMAIL=My Blog <myblog@gmail.com>
-DB_NAME=blog
-DB_USER=blog
-DB_PASSWORD=blog
-DB_HOST=localhost
-SECRET_KEY=django-insecure-bq817^e5q$_51d-kw2^d&(fre94+e_-ovygpskq)#*a%3ww#v_
-postgresql://blog:h7xZr3Ms5WDabJdo9ioijhrua6TEK3sf@dpg-d5svnh5actks73a66n3g-a.oregon-postgres.render.com/blog_dktk
